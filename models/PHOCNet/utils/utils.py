d8a7605c981bc2aa8ddafc444324b34f319f5e8a
marinocom <marino.oliverosblanco@gmail.com>
2024-05-21 11:38:15 +0200

    creation of PHOCNet pipeline

---
+++ models/PHOCNet/utils/utils.py
@@ -0,0 +1,89 @@
+import numpy as np
+import os
+import torch
+import torch.nn 
+from torchvision import transforms
+from models.PHOCNet import *
+
+from torch.optim.lr_scheduler import StepLR, CyclicLR, CosineAnnealingLR, ReduceLROnPlateau
+from torchvision import datasets, models, transforms
+
+from .build_phoc import phoc
+from .dataset import dataset
+
+
+def get_data(img_dir, transform=None):
+
+    dataset_ = dataset(img_dir, transform)
+
+    return dataset_
+
+def make_loader(dataset, batch_size):
+    loader = torch.utils.data.DataLoader(dataset=dataset,
+                                         batch_size=batch_size, 
+                                         shuffle=True,
+                                         pin_memory=True, num_workers=2)
+    return loader
+
+def make(config, device="cuda"):
+
+    transforms_train = transforms.Compose([
+        transforms.Resize((64, 128), antialias=True),
+        transforms.Normalize(mean=[0.445313568], std=[0.26924618])
+    ])
+
+    transforms_test = transforms.Compose([
+        transforms.Resize((64, 128), antialias=True),
+        transforms.Normalize(mean=[0.445313568], std=[0.26924618])
+    ])
+
+    train, test = get_data(config.train_dir, transforms_train), get_data(config.test_dir, transforms_test)
+
+    train_loader = make_loader(train, config.batch_size)
+    test_loader = make_loader(test, config.batch_size)
+
+    # Make the model
+    model = PHOCNet(n_out = train[0][1].shape[0], input_channels = 1).to(device)
+    model.apply(init_weights_model)
+    """model = models.resnet18(pretrained=True) 
+    set_parameter_requires_grad(model,True)
+    model.fc = nn.Sequential(nn.Linear(512, 512),
+                             nn.ReLU(),
+                             nn.Linear(512, 512),
+                             nn.ReLU(),
+                             nn.Linear(512, 512),
+                             nn.ReLU(),
+                             nn.Linear(512, train[0][1].shape[0]))
+    model=model.to(device)"""
+    
+    pos_weight = torch.tensor(create_weights(config.train_dir)).to(device)
+    criterion = torch.nn.BCEWithLogitsLoss(reduction = 'mean', pos_weight=pos_weight)
+
+    optimizer = torch.optim.SGD(model.parameters(), lr=config.learning_rate, momentum=0.9)
+    #optimizer = torch.optim.Adam(model.parameters(), lr=config.learning_rate)
+    scheduler = StepLR(optimizer, step_size=2, gamma=0.3)
+    #scheduler = CosineAnnealingLR(optimizer, T_max=10)
+    #scheduler = ReduceLROnPlateau(optimizer, patience = 2, factor = 0.1)
+    
+    return model, train_loader, test_loader, criterion, optimizer, scheduler
+
+def init_weights_model(m):
+        if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
+            nn.init.kaiming_normal_(m.weight)
+            if m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+
+def create_weights(file_words):
+    paths = os.listdir(file_words)
+    list_of_words = [path.split("_")[-1].split(".")[0] for path in paths]
+    list_of_words = [l[:-1] for l in list_of_words]
+    phoc_representations = phoc(list_of_words)
+    suma = np.sum(phoc_representations, axis=0)
+    weights = phoc_representations.shape[0]/(suma+1e-6) 
+    weights = (1 + (weights/max(weights)))*5
+    return weights
+
+def set_parameter_requires_grad(model, feature_extracting):
+        if feature_extracting:
+            for param in model.parameters():
+                param.requires_grad = False
---
+++ models/PHOCNet/utils/wandb_logs.py
@@ -0,0 +1,43 @@
+import wandb
+from torchvision import transforms as T
+from PIL import ImageDraw, ImageFont
+from torchvision import transforms
+
+
+def train_log(loss, total_example_ct):
+    wandb.log({"loss": loss}, step=total_example_ct)
+    print(f"Loss after {str(total_example_ct).zfill(5)} examples: {loss:.3f}")
+
+def train_test_log(loss_test, loss_train, accuracy_test, accuracy_train, edit_test, edit_train, epoch):
+    wandb.log({"Epoch": epoch, 
+               "Train loss": loss_train, "Test loss": loss_test,
+               "Train accuracy": accuracy_train, "Test accuracy": accuracy_test,
+               "Train edit": edit_train, "Test edit": edit_test,
+               })
+    print(f"Train Loss: {loss_train:.3f}\nTest Loss: {loss_test:.3f}")
+
+def log_images(images, predicted_labels, text_labels, epoch, mode):
+    t = transforms.Compose([transforms.Normalize(0, 1/0.1),transforms.Normalize(-0.5, 1)])
+    t_images = t(images)
+    images_with_labels = draw_images(t_images, text_labels, predicted_labels)
+    wandb.log({f"Epoch{epoch}-{mode}": [wandb.Image(im) for im in images_with_labels]})
+
+def draw_images(images, text_labels, predicted_labels):
+    transform = T.ToPILImage()
+    images = [draw_one_image(transform(im), t_lab, p_lab) for im, t_lab, p_lab in zip(images, text_labels, predicted_labels)]
+    return images
+
+def draw_one_image(image, text_label, predicted_label):
+    image = image.convert("RGB")
+    draw = ImageDraw.Draw(image)
+    if text_label == predicted_label:
+        color = "green"
+    else:
+        color = "red"
+    text = text_label + "\n" + predicted_label
+    font = ImageFont.truetype(f'/home/alumne/xnap-project-ed_group_01/generate_images/fonts/ARIAL.TTF', 10)
+    draw.text((0,0), text, font=font, fill = color)
+    return image
+
+def lr_log(lr, epoch):
+    wandb.log({"learning-rate": lr}, step=epoch)
